f4e9e1bd93f9b9c18d778a2896a4808bef71d380
Shrey Naik <61283238+ShreyNaik123@users.noreply.github.com>
2024-01-13 12:57:54 +0530

    fixed stats spacing

--- src/components/Stats/UserStats.js
+++ src/components/Stats/UserStats.js
@@ -1,38 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function UserStats({ sessions, sessionTime }) {
-	function convertTime(seconds) {
-		if (seconds >= 3600) {
-			const hours = Math.floor(seconds / 3600);
-			const remainingMinutes = Math.floor((seconds % 3600) / 60);
-			return `${hours} hours and ${remainingMinutes} minutes`;
-		} else {
-			const minutes = Math.floor(seconds / 60);
-			return `${minutes}`;
-		}
-	}
-
 	const stats = [
 		{ id: 1, name: "Sessions Today", value: sessions },
 		{
 			id: 2,
 			name: `Minutes\nTime Focused Today`,
-			value: sessions
-				? convertTime(sessions * sessionTime)
-				: convertTime(sessionTime),
+			value: sessions ? sessions * sessionTime : sessionTime,
 		},
 	];
 
+	// Set a maximum height for the dd element
+	const maxHeight = 210;
 	return (
 		<div className="bg-white py-5 w-100">
 			<div className="container">
 				<div className="row justify-content-center">
 					{stats.map((stat) => (
 						<div key={stat.id} className="col-md-6 my-3">
-							<dl className="text-center">
+							<dl className="text-center d-flex flex-column justify-content-between h-100">
 								<dd
-									className="text-3xl font-weight-bold text-gray-900"
-									style={{ fontSize: "xxx-large" }}
+									className="font-weight-bold text-gray-900"
+									style={{
+										maxHeight: `${maxHeight}px`,
+										overflow: "hidden",
+										fontSize: "xxx-large",
+									}}
 								>
 									{stat.value}
 								</dd>
